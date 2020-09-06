b2970244bc81863aee5442b39cf50b01d7a9bab1
HTMLProgrammer2001 <cssuperpy@gmail.com>
2020-09-07 02:56:46 +0300

    Link edit

--- Common/config.py
+++ Common/config.py
@@ -11,14 +11,14 @@ dbConnection = {
 }
 
 # communication
-site_url = 'https://magazinel.herokuapp.com/'
+site_url = os.environ.get('SITE_URL') or 'https://magazinel.herokuapp.com/'
 mail = 'cssuperpy@gmail.com'
 phone = '+380666754334'
 
 # API
-api_base_url = os.environ.get('API_URL') or 'https://magazinel.herokuapp.com/'
-api_get_filters = f'{api_base_url}api/productFilters'
-api_get_products = f'{api_base_url}api/getProducts'
-api_find = f'{api_base_url}api/find'
+api_base_url = os.environ.get('API_URL') or 'https://magazinel.herokuapp.com/api'
+api_get_filters = f'{api_base_url}/productFilters'
+api_get_products = f'{api_base_url}/getProducts'
+api_find = f'{api_base_url}/find'
 
-images_base_url = f'{api_base_url}image'
+images_base_url = f'{site_url}image'
--- Common/helpers.py
+++ Common/helpers.py
@@ -66,9 +66,13 @@ def sendProduct(userID: str, product: dict):
         loads(product['sizes']) if isinstance(product['sizes'], str) else product['sizes']
     )
 
-    reply = keyboards.linkKeyboard(product['name'], 'https://google.com')
+    reply = keyboards.linkKeyboard(product['name'], f"{config.site_url}products/{product['slug']}")
 
-    bot.send_photo(userID, f"{config.images_base_url}/{product['photo']}")
+    bot.send_photo(
+        userID, 
+        f"{config.images_base_url}/{product['photo']}", 
+        reply_markup=keyboards.removeKeyBoard()
+    )
     #bot.send_photo(userID, 'https://i.insider.com/5eda82ae3ad8617d4e1c0b2e?width=1100&format=jpeg&auto=webp')
 
     bot.send_message(
